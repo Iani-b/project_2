76479d0921f6e416d6aaa1dd18e93904ef6ae6a1
Ianis B <ianis.primary@gmail.com>
2025-10-15 10:03:45 +0100

    minor

--- app.py
+++ app.py
@@ -2,9 +2,22 @@ from flask import Flask, render_template, jsonify, request, redirect
 import os
 import time
 import json
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/signup", methods = "POST")
+def signup():
+    pass
+
+@app.route("/login")
+def login():
+    pass
+
+@app.route("/game", methods = "POST")
+def game():
+    pass
