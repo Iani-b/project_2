c7a051e2a86831818f34476de25d5aa4a2bbe392
Ianis B <ianis.primary@gmail.com>
2025-10-15 10:34:22 +0100

    fix this

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 - For A-Levels
 - This time it is web-based
 - Using Flask web framework with html, css and javascript
+- ong I hate css i'm crine 🎋🎋🎋
 
 ## Flowchart...
 
--- app.py
+++ app.py
@@ -8,16 +8,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    
     return render_template("home.html")
 
-@app.route("/signup", methods = "POST")
+@app.route("/signup", methods = ["POST"])
 def signup():
+    
     pass
 
 @app.route("/login")
 def login():
+    
     pass
 
-@app.route("/game", methods = "POST")
+@app.route("/game", methods = ["POST"])
 def game():
+    
     pass
+
+app.run(debug=True)
